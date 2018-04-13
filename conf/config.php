515914d2cdd87407c697684aaf800012d3d56770
answer2c <answerybc@163.com>
2018-04-13 17:04:14 +0800

    413

---
+++ conf/config.php
@@ -0,0 +1,7 @@
+<?php
+
+    return [
+        'app_debug'              => true,
+        'app_author' => 'ybc',   
+	 'view_replace_str'       => ['SITE_URL'=>'http://answer2c.cn'],
+    ];
---
+++ conf/route.php
@@ -0,0 +1,9 @@
+<?php
+   use \think\Route;
+ 
+ Route::get('/Mess','index/index/mess');
+
+ Route::get('/index','index/index/index');
+
+ 
+
